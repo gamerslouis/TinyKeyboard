ecf3bedea1b2935e4e571abe4404823c636a0e24
gamerslouis <32395144+gamerslouis@users.noreply.github.com>
2018-09-13 15:45:01 +0800

    RD

---
+++ CenterControlGUIMessage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinyKeyboard
+{
+    class CenterControlGUIMessage
+    {
+        private CenterControl cc;
+
+        public CenterControlGUIMessage(CenterControl cc)
+        {
+            this.cc = cc;
+        }
+
+        public JSONProfile GetCurrentProfile()
+        {
+            return cc.profileContainer.jSONProfiles[cc.profileContainer.ProfileIndex];
+        }
+
+        public int GetCureentIndex()
+        {
+            return cc.profileContainer.ProfileIndex;
+        }
+
+        public string[] GetProfileNames()
+        {
+            string[] names = new String[cc.profileContainer.jSONProfiles.Length];
+            for (int i = 0; i < cc.profileContainer.jSONProfiles.Length; i++)
+            {
+                names[i] = cc.profileContainer.jSONProfiles[i].Name;
+            }
+            return names;
+        }
+
+
+    }
+}
